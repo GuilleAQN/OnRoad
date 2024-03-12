5ca466e13312886d1771a3f662bb1cb03430a396
GuilleAQN <omarguillermo1@gmail.com>
2024-03-12 09:54:14 -0400

    Primer Commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse,JsonResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+
+
 def index(request):
-    title= "OnRoad"
-    return render(request,"index.html",{
-        "title":title,
+    title = "OnRoad"
+    return render(request, "index.html", {
+        "title": title,
     })
+
+
 def hello(request):
     return HttpResponse("<h1>Hola mundo</h1>")
 
+
 def about(request):
     return HttpResponse("<h1>About</h1>")
-
-
